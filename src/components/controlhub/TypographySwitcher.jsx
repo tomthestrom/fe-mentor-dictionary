c6465763b9408768d65920086291365f08fac024
tomthestrom <thruska29@gmail.com>
2023-05-25 17:29:04 +0200

    TypographySwitcher - font switching working as required added hooks/useOutsideClick - upon click outside the elements contained _typography.switcher.css - some styles by the "ref"-ed element calls the provided callback - source in the file

--- src/components/controlhub/TypographySwitcher.jsx
+++ src/components/controlhub/TypographySwitcher.jsx
@@ -1,6 +1,7 @@
 import TypographyItem from "./TypographyItem";
 import Arrow from "../svg/Arrow";
 import { useState } from "react";
+import useOutsideClick from "../../hooks/useOutsideClick";
 /**
  * @TODO: Maybe put it into some config?
  */
@@ -32,14 +33,12 @@ function TypographySwitcher() {
   const [activeFont, setActiveFont] = useState(
     fonts.find((font) => font.active)
   );
-  const [showInactiveFonts, setShowInactiveFonts] = useState(false);
-
+  const [showFontList, setShowFontList] = useState(false);
   const inActiveFonts = fonts.filter((font) => font.id !== activeFont.id);
 
   const getFontById = (id) => fonts.find((font) => font.id === id);
 
   const handleLabelClick = (e, id) => {
-    // setShowInactiveFonts(false);
     const nextFont = getFontById(id);
     setActiveFont(nextFont);
     setBodyFont(nextFont.cssFontFamily);
@@ -47,8 +46,11 @@ function TypographySwitcher() {
 
   const handleBtnClick = (e) => {
     e.preventDefault();
-    setShowInactiveFonts(!showInactiveFonts);
+    toggleFontList();
   };
+
+  const toggleFontList = () => setShowFontList(!showFontList);
+  const hideFontList = () => setShowFontList(false);
 
   const setBodyFont = (fontFamilyValue) => {
     document.documentElement.style.setProperty(
@@ -57,26 +59,40 @@ function TypographySwitcher() {
     );
   };
 
+  const switcherRef = useOutsideClick(hideFontList);
+
   return (
-    <form className="typography-switcher">
+    <form className="typography-switcher" ref={switcherRef}>
       <TypographyItem
         id={activeFont.id}
         name={activeFont.name}
-        handleLabelClick={handleLabelClick}
-      ></TypographyItem>
-      {inActiveFonts.map((font) => {
-        return (
-          <TypographyItem
-            key={font.id}
-            id={font.id}
-            name={font.name}
-            handleLabelClick={handleLabelClick}
-          />
-        );
-      })}
+        handleLabelClick={toggleFontList}
+      />
       <button className="typography-switcher__arrow" onClick={handleBtnClick}>
         <Arrow />
       </button>
+      {showFontList && (
+        <ul className="typography-switcher__list" >
+          <li key={activeFont.id} className="typography-switcher__list-item">
+            <TypographyItem
+              id={activeFont.id}
+              name={activeFont.name}
+              handleLabelClick={toggleFontList}
+            />
+          </li>
+          {inActiveFonts.map((font) => {
+            return (
+              <li key={font.id} className="typography-switcher__list-item">
+                <TypographyItem
+                  id={font.id}
+                  name={font.name}
+                  handleLabelClick={handleLabelClick}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </form>
   );
 }

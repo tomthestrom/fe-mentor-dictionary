ead9b2f9a80ada22895bb100a70199c3fc567596
tomthestrom <thruska29@gmail.com>
2023-05-25 13:58:14 +0200

    _typography.scss - fonts added via @font-face, font-family of body set via a variable (controlled via js - TypographySwitcher) TypographyItem, TypographySwitcher - js for switching the typography of the app implemented TODO: style - show fonts in a list that opens on button(arrow-down) click

--- src/components/controlhub/TypographyItem.jsx
+++ src/components/controlhub/TypographyItem.jsx
@@ -1,7 +1,8 @@
-function TypographyItem({id, name}) {
+function TypographyItem({id, name, handleLabelClick}) {
+
   return (
         <>
-            <label className="typography-switcher__label" htmlFor={id}>{name}</label> 
+            <label onClick={(e) => handleLabelClick(e, id)} className="typography-switcher__label" htmlFor={id}>{name}</label> 
             <input className="typography-switcher__radio" type="radio" name="font-name" id={id}/>
           </>
   )
--- src/components/controlhub/TypographySwitcher.jsx
+++ src/components/controlhub/TypographySwitcher.jsx
@@ -1,34 +1,82 @@
 import TypographyItem from "./TypographyItem";
 import Arrow from "../svg/Arrow";
+import { useState } from "react";
 /**
  * @TODO: Maybe put it into some config?
  */
+
+const BODY_FONT_CUSTOM_PROP = "--default-font-family";
 
 const fonts = [
   {
     id: "mono",
     name: "Mono",
     active: true,
+    cssFontFamily: '"Inconsolata", monospace',
   },
   {
     id: "sans-serif",
     name: "Sans Serif",
     active: false,
+    cssFontFamily: '"Inter", sans-serif',
   },
   {
     id: "serif",
     name: "Serif",
     active: false,
+    cssFontFamily: '"Lora", serif',
   },
 ];
 
 function TypographySwitcher() {
-  const activeFont = fonts.find(font => font.active);
+  const [activeFont, setActiveFont] = useState(
+    fonts.find((font) => font.active)
+  );
+  const [showInactiveFonts, setShowInactiveFonts] = useState(false);
+
+  const inActiveFonts = fonts.filter((font) => font.id !== activeFont.id);
+
+  const getFontById = (id) => fonts.find((font) => font.id === id);
+
+  const handleLabelClick = (e, id) => {
+    // setShowInactiveFonts(false);
+    const nextFont = getFontById(id);
+    setActiveFont(nextFont);
+    setBodyFont(nextFont.cssFontFamily);
+  };
+
+  const handleBtnClick = (e) => {
+    e.preventDefault();
+    setShowInactiveFonts(!showInactiveFonts);
+  };
+
+  const setBodyFont = (fontFamilyValue) => {
+    document.documentElement.style.setProperty(
+      BODY_FONT_CUSTOM_PROP,
+      fontFamilyValue
+    );
+  };
 
   return (
     <form className="typography-switcher">
-      <TypographyItem id={activeFont.id} name={activeFont.name}></TypographyItem>
-      <button className="typography-switcher__arrow"><Arrow /></button>
+      <TypographyItem
+        id={activeFont.id}
+        name={activeFont.name}
+        handleLabelClick={handleLabelClick}
+      ></TypographyItem>
+      {inActiveFonts.map((font) => {
+        return (
+          <TypographyItem
+            key={font.id}
+            id={font.id}
+            name={font.name}
+            handleLabelClick={handleLabelClick}
+          />
+        );
+      })}
+      <button className="typography-switcher__arrow" onClick={handleBtnClick}>
+        <Arrow />
+      </button>
     </form>
   );
 }

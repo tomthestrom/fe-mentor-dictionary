ae61ea4bd267ccf3409fffa834f66f35b7887951
tomthestrom <thruska29@gmail.com>
2023-05-27 12:11:07 +0200

    Border of search-results and design lines after pof-speech__function added via borders on pseudo-elements Theme config stuff to handle border color changes added

--- src/config/themes.js
+++ src/config/themes.js
@@ -5,6 +5,7 @@ const SEARCH_BG_CLR = "--search-bar-bg-color";
 const SEARCH_PHOLDER_CLR = "--search-bar-placeholder-color";
 const DRP_DWN_BG_CLR = "--drp-dwn-bg-color";
 const DRP_DWN_B_SHDW = "--drp-dwn-b-shadow";
+const BRDR_CLR = "--border-color";
 
 
 const lightScheme = new Map();
@@ -16,6 +17,7 @@ lightScheme.set(SEARCH_BG_CLR, "var(--color-grey-F4)");
 lightScheme.set(SEARCH_PHOLDER_CLR, "var(--color-grey-75)");
 lightScheme.set(DRP_DWN_BG_CLR, "var(--main-bg-color)");
 lightScheme.set(DRP_DWN_B_SHDW, "var(--box-shadow-dark)");
+lightScheme.set(BRDR_CLR, "var(--color-grey-E9)");
 
 darkScheme.set(MAIN_BG_CLR, "var(--color-black-05)");
 darkScheme.set(MAIN_FONT_CLR, "var(--color-white-FF)");
@@ -23,5 +25,6 @@ darkScheme.set(SEARCH_BG_CLR, "var(--color-black-2D)");
 darkScheme.set(SEARCH_PHOLDER_CLR, "var(--color-grey-F4)");
 darkScheme.set(DRP_DWN_BG_CLR, "var(--search-bar-bg-color)");
 darkScheme.set(DRP_DWN_B_SHDW, "var(--box-shadow-purple)");
+darkScheme.set(BRDR_CLR, "var(--color-black-3A)");
 
 export { lightScheme as lightSchemeCfg, darkScheme as darkSchemeCfg };

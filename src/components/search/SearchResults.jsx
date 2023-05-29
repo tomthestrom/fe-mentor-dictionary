ba070e42f0142761524db609ebce448cc9cfb9c3
tomthestrom <thruska29@gmail.com>
2023-05-29 02:22:39 +0200

    SearchResults - some edge cases handling - long inputs, no audio = no btn, due to some strange default phonetic transcription eg. for the word 'actually' -> we're looking for the longest one and using it instead
    some css fixes - margin of search-bar removed and instead padding added
    to potential children

--- src/components/search/SearchResults.jsx
+++ src/components/search/SearchResults.jsx
@@ -9,21 +9,28 @@ function SearchResults({ props }) {
   const handleClick = () => {
     audioRef.current.play();
   };
-  /**
-   * used for retrieving audioUrl and phonetic transcription
-   * Expected val: "audio" || "text"
-   */
-  const getFromPhonetics = (props, val) => {
-    if (props.phonetics === undefined || props.phonetics.length === 0) {
-      return null;
-    }
-    return props.phonetics.find(
-      (item) => item[val] !== undefined && item[val].length > 0
-    )[val];
-  };
 
-  const phonetic = props.phonetic ?? getFromPhonetics(props, "text");
-  const audioUrl = getFromPhonetics(props, "audio");
+  const defaultPhonetic = props.phonetic ?? ""; 
+
+  /**
+   * Sometimes the default value (props.phonetic) is a "strange one"
+   * consisting of too few chars
+   * here we check whether there's a longer phonetic transcription
+   */
+  const phoneticTrans =  props?.phonetics?.reduce((acc, val) => {
+    if (val.text && val.text.length > acc.length) {
+      acc = val.text;
+    }
+    return acc;
+  }, defaultPhonetic);
+
+  const audioUrl = props?.phonetics?.reduce((url, item) => {
+    if (item.audio && item.audio.length > 0) {
+      url = item.audio;
+    }
+    return url;
+  }, null);
+
   const sourceUrl = props?.sourceUrls[0];
 
   return (
@@ -31,22 +38,28 @@ function SearchResults({ props }) {
       <main className="search-results">
         <header className="word-forms">
           <h1 className="word-forms__word">{props.word}</h1>
-          <figcaption className="word-forms__spoken">
-            <figcaption className="word-forms__phonetic">{phonetic}</figcaption>
-            <audio
-              ref={audioRef}
-              id="word-recording"
-              src={audioUrl}
-              className="word-forms__audio"
-            ></audio>
-          </figcaption>
-          <button
-            onClick={handleClick}
-            aria-controls="word-recording"
-            className="word-forms__play"
-          >
-            <Play />
-          </button>
+          {phoneticTrans && (
+            <figcaption className="word-forms__spoken">
+              <figcaption className="word-forms__phonetic">
+                {phoneticTrans}
+              </figcaption>
+              <audio
+                ref={audioRef}
+                id="word-recording"
+                src={audioUrl}
+                className="word-forms__audio"
+              ></audio>
+            </figcaption>
+          )}
+          {audioUrl && (
+            <button
+              onClick={handleClick}
+              aria-controls="word-recording"
+              className="word-forms__play"
+            >
+              <Play />
+            </button>
+          )}
         </header>
         {props.meanings.map((partOfSpeech) => (
           <PartOfSpeech key={partOfSpeech.partOfSpeech} props={partOfSpeech} />
